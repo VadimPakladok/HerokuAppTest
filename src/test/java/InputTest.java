6c5fa3a95656ecbae13e7bfa74d30bb304ab049f
Vadim <Vadim8730@gmail.com>
2021-05-05 16:00:36 +0300

    Input test Update.

--- src/test/java/InputTest.java
+++ src/test/java/InputTest.java
@@ -1,4 +1,7 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
@@ -7,17 +10,12 @@ public class InputTest extends BaseTest{
     @Test
     public void inputTest() {
         driver.get("http://the-internet.herokuapp.com/inputs");
-        driver.findElement(By.tagName("input")).sendKeys("1");
-        driver.findElement(By.tagName("input")).clear();
-        driver.findElement(By.tagName("input")).sendKeys("aaa");
-        driver.findElement(By.tagName("input")).clear();
-        driver.findElement(By.tagName("input")).sendKeys("Keys.ARROW_UP");
-        driver.findElement(By.tagName("input")).sendKeys("Keys.ARROW_UP");
-        driver.findElement(By.tagName("input")).sendKeys("Keys.ARROW_UP");
-        driver.findElement(By.tagName("input")).sendKeys("Keys.ARROW_UP");
-//        public void arrowUp() {
-//            driver.findElement(By.tagName("input")).sendKeys("Keys.ARROW_UP");
-//        }
+        WebElement input = driver.findElement(By.tagName("input"));
+        input.sendKeys("10");
+        input.sendKeys(Keys.ARROW_UP);
+        assertEquals(input.getAttribute("value"), "11", "ARROW UP IS NOT WORK");
+        input.sendKeys(Keys.ARROW_DOWN);
+        assertEquals(input.getAttribute("value"),"10", "ARROW DOWN IS NOT WORK");
 
 
     }
